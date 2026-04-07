ff10cf7cb128d6f0ce035be5086aa716257fc42a
Rik Visser <rik@sonicverse.eu>
2026-04-07 11:58:34 +0200

    feat: update layout and styles, add TypeScript configuration and VSCode settings
    - Import global CSS in Layout.astro and change the title to "Sonicverse".
    - Update tsconfig.json to include JSX support for React.
    - Add new VSCode settings for Snyk organization selection.
    - Create global CSS file and import Tailwind CSS.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,23 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import node from '@astrojs/node';
+
+import mdx from '@astrojs/mdx';
+
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), mdx()],
+
+  adapter: node({
+    mode: 'standalone'
+  }),
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
